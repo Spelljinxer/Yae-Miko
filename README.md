be784031bf69bc886b9ec07e49f5e662a83dcfb7
Spelljinxer <35420739+Spelljinxer@users.noreply.github.com>
2022-06-19 23:10:06 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Yae-Miko
+# Spelljinxer.github.io - placeholder for Yae Miko site
+> this is just a boredom killer
+
+# TODO
+- [ ] F2P Build Section
+- [ ] Premium Build Section
+- [ ] Find out what else to put
+- [ ] Maybe a third build?
+- [ ] Dropdown menu for builds
+
---
+++ script.js
@@ -0,0 +1,70 @@
+
+
+//---------------------------------------------------------------------------------
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 300;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+window.addEventListener("scroll", reveal);
+
+window.addEventListener("scroll", scrollFunction);
+
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+function scrollFunction() {
+  if (window.pageYOffset > 950) { 
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else {
+    if(backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 1;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
